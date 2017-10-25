3b6b16f1a1ff039e33e30839d739d38e74c2a4ac
youssefgirgeis <youssefgirgeis@gmail.com>
2017-10-24 22:51:13 -0400

    updated comic book model

--- ComicBookGallary/Models/ComicBook.cs
+++ ComicBookGallary/Models/ComicBook.cs
@@ -13,5 +13,21 @@ namespace ComicBookGallary.Models
         public string DescriptionHTML { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
