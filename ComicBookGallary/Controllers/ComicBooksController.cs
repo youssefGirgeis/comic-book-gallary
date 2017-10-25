bd3a8cb8d077596e924046c766b62cfbfef7a929
youssefgirgeis <youssefgirgeis@gmail.com>
2017-10-24 23:17:03 -0400

    updated Comic Book Detail View to be strongly typed

--- ComicBookGallary/Controllers/ComicBooksController.cs
+++ ComicBookGallary/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,22 @@ namespace ComicBookGallary.Controllers
 
         public ActionResult Detail()
         {
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist (){Name="Dan Slott", Role="Script"},
+                    new Artist (){Name="Humberto Ramos", Role="Pencils"},
+                    new Artist (){Name=" Victor Olazaba", Role="Inks"},
+                    new Artist (){Name="Edgar Delgado", Role="Colors"},
+                    new Artist (){Name="Chris Eliopoulos", Role="Letters"},
+                }
+
+
+            };
 
             /* if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
              {
@@ -19,19 +36,7 @@ namespace ComicBookGallary.Controllers
 
              return Content("Hello Youssef");*/
 
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-
-            return View();
+            return View(comicBook);
         }
     }
 }
